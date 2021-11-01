1ca67d025eb8df21adb7777d79c25812e7ff7a26
unpaliseulois <unpaliseuloisdev@hotmail.com>
2021-11-01 23:44:41 +0100

    Move precedences to SyntaxFacts.cs

--- SmartCalc/Global/CodeAnalysis/Parser.cs
+++ SmartCalc/Global/CodeAnalysis/Parser.cs
@@ -60,8 +60,8 @@ namespace SmartCalc.Global.CodeAnalysis
             var left = ParsePrimaryExpression();
 
             while (true)
-            {
-                var precedence = GetBinaryOperatorPrecedence(Current.Kind);
+            {                
+                var precedence = Current.Kind.GetBinaryOperatorPrecedence();
 
                 if (precedence == 0 || precedence <= parentPrecedence)
                     break;
@@ -72,20 +72,7 @@ namespace SmartCalc.Global.CodeAnalysis
             }
             return left;
         }
-        private static int GetBinaryOperatorPrecedence(SyntaxKind kind)
-        {
-            switch (kind)
-            {
-                case SyntaxKind.StarToken:
-                case SyntaxKind.SlashToken:
-                    return 2;
-                case SyntaxKind.PlusToken:
-                case SyntaxKind.MinusToken:
-                    return 1;                
-                default:
-                    return 0;
-            }
-        }
+        
         private ExpressionSyntax ParsePrimaryExpression()
         {
             if (Current.Kind == SyntaxKind.OpenParenthsisToken)
