473e596437eec7286ead416c3286c4bf3fbb5088
unpaliseulois <unpaliseuloisdev@hotmail.com>
2021-11-01 23:24:40 +0100

    Make precedence data-dr

---
+++ auto-git.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ "$1" != "" ] && [ "$2" != "" ]
+then
+	git status
+	git add .
+	git status
+	git commit "$1"
+	git push -u origin "$2"
+else
+	if [ "$1" == "" ]
+	then
+		echo "Commit message is missing."
+
+	elif [ "$2" == "" ]
+	then
+		echo "Git branch to push modification in is missing."
+	fi
+fi
