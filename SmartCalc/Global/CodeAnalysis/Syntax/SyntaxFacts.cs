986a955eb278556723632a0480370e9ff09c0939
unpaliseulois <unpaliseuloisdev@hotmail.com>
2021-11-05 04:10:55 +0100

    Add SyntaxFacts.GetUnaryOperatorKinds() and SyntaxFacts.GetBinaryOperatorKinds()

--- SmartCalc/Global/CodeAnalysis/Syntax/SyntaxFacts.cs
+++ SmartCalc/Global/CodeAnalysis/Syntax/SyntaxFacts.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace SmartCalc.Global.CodeAnalysis.Syntax
 {
@@ -51,6 +52,24 @@ namespace SmartCalc.Global.CodeAnalysis.Syntax
                     return SyntaxKind.TrueKeyword;
                 default:
                     return SyntaxKind.IdentifierToken;
+            }
+        }
+        public static IEnumerable<SyntaxKind> GetUnaryOperatorKinds()
+        {
+            var kinds = (SyntaxKind[])Enum.GetValues(typeof(SyntaxKind));
+            foreach (var kind in kinds)
+            {
+                if (GetUnaryOperatorPrecedence(kind) > 0)
+                    yield return kind;
+            }
+        }
+        public static IEnumerable<SyntaxKind> GetBinaryOperatorKinds()
+        {
+            var kinds = (SyntaxKind[])Enum.GetValues(typeof(SyntaxKind));
+            foreach (var kind in kinds)
+            {
+                if (GetBinaryOperatorPrecedence(kind) > 0)
+                    yield return kind;
             }
         }
         public static string GetText(SyntaxKind kind)
