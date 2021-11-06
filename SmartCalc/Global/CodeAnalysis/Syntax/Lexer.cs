78706b69f69344aea4c61ba758b250418368cd5e
unpaliseulois <unpaliseuloisdev@hotmail.com>
2021-11-06 02:12:02 +0100

    More clean-up and Lex method read number token simplification.

--- SmartCalc/Global/CodeAnalysis/Syntax/Lexer.cs
+++ SmartCalc/Global/CodeAnalysis/Syntax/Lexer.cs
@@ -139,13 +139,23 @@ namespace SmartCalc.Global.CodeAnalysis.Syntax
                         _kind = SyntaxKind.EqualsToken;
                     }
                     break;
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    {
+                        ReadNumberToken();
+                    }
+                    break;
                 default:
                     {
-                        if (char.IsDigit(Current))
-                        {
-                            ReadNumberToken();
-                        }
-                        else if (char.IsWhiteSpace(Current))
+                        if (char.IsWhiteSpace(Current))
                         {
                             ReadWhiteSpaceToken();
                         }
